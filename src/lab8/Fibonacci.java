e74d503c7fd8309d3d50fff2aff07de52343889c
Raul Soto <potun3000@yahoo.com>
2023-06-27 10:18:08 -0700

    Created Fibonacci.java

---
+++ src/lab8/Fibonacci.java
@@ -0,0 +1,24 @@
+package lab8;
+
+import java.util.ArrayList;
+
+public class Fibonacci {
+    private static ArrayList<Integer> fibonacciList = new ArrayList<>();
+    public static void main(String[] args) {
+        fibonacciList.add(0);
+        fibonacciList.add(1);
+        System.out.println(fibonacci(100));
+        System.out.println(fibonacciList);
+    }
+    public static int fibonacci(int n) {
+        if(n < 2) {
+            return n;
+        } else if (fibonacciList.size() > n) {
+            return fibonacciList.get(n);
+        } else {
+            fibonacciList.add(fibonacci(n - 2) + fibonacci(n - 1));
+            return fibonacciList.get(n);
+        }
+
+    }
+}
